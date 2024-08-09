483e8fb1b66c4587c16d99b2d57c077374f3b29a
drag0n1zed <62168687+drag0n1zed@users.noreply.github.com>
2024-08-09 14:35:53 +0900

    Initial commit

---
+++ src/client/java/drag0n1zed/biologic/client/BiologicClient.java
@@ -0,0 +1,10 @@
+package drag0n1zed.biologic.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BiologicClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/drag0n1zed/biologic/Biologic.java
@@ -0,0 +1,10 @@
+package drag0n1zed.biologic;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Biologic implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
